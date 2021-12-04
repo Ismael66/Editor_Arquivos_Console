d1411698aaa2572246ca1b4544dbe314ecae1af7
Ismael Mateus <ismaelbnt66@gmail.com>
2021-12-03 22:02:04 -0300

    Básico completo

--- Program.cs
+++ Program.cs
@@ -7,70 +7,122 @@ namespace Criador
 {
     class Program
     {
-        static string arquivoEscolhido = @"\Teste.txt";
+        static string arquivoEscolhido = @"\teste.txt";
+        static string path = Directory.GetCurrentDirectory();
         public static void Main()
         {
-            Console.Clear();
-            Console.WriteLine("=====================================================================");
-            Console.WriteLine("Menu Principal");
-            Console.WriteLine("=====================================================================");
-            Console.WriteLine("[1] CRIAR ARQUIVO");
-            Console.WriteLine("[2] INSERIR DADOS");
-            Console.WriteLine("[3] MUDAR ARQUIVO");
-            Console.WriteLine("=====================================================================");
-            Console.Write("Escolha: ");
-            int escolha = int.Parse(Console.ReadLine());
-            Acao(escolha);
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("=====================================================================");
+                Console.WriteLine("Menu Principal");
+                Console.WriteLine("=====================================================================");
+                Console.WriteLine("[1] CRIAR ARQUIVO");
+                Console.WriteLine("[2] INSERIR DADOS");
+                Console.WriteLine("[3] VISUALIZAR ARQUIVO");
+                Console.WriteLine("[4] MUDAR ARQUIVO");
+                Console.WriteLine("=====================================================================");
+                Console.Write("Escolha: ");
+                int escolha = int.Parse(Console.ReadLine());
+                Acao(escolha);
+            }
+            catch (System.Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("=====================================================================");
+                Console.WriteLine("Escolha uma opção valida.");
+                Console.WriteLine("=====================================================================");
+                System.Threading.Thread.Sleep(1000);
+                Main();
+            }
         }
         public static void Acao(int escolha)
         {
-
-            switch (escolha)
+            try
             {
-                case 1:
-                    Console.Clear();
-                    Console.WriteLine("=====================================================================");
-                    Console.Write("ESCOLHA O TITULO DO ARQUIVO: ");
-                    string titulo = Console.ReadLine();
-                    arquivoEscolhido = @$"\{titulo}.txt";
-                    var path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + arquivoEscolhido;
-                    if (File.Exists(path))
-                    {
+                switch (escolha)
+                {
+
+                    case 1:
                         Console.Clear();
                         Console.WriteLine("=====================================================================");
-                        Console.WriteLine("JÁ EXISTE UM ARQUIVO COM O MESMO NOME.");
+                        Console.Write("ESCOLHA O TITULO DO ARQUIVO: ");
+                        arquivoEscolhido = @$"\{Console.ReadLine()}.txt";
+                        if (File.Exists(path + arquivoEscolhido))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("=====================================================================");
+                            Console.WriteLine("JÁ EXISTE UM ARQUIVO COM O MESMO NOME.");
+                            Console.WriteLine("=====================================================================");
+                            System.Threading.Thread.Sleep(1000);
+                            Main();
+                        }
+                        else
+                        {
+                            StreamWriter arquivo = new StreamWriter(path + arquivoEscolhido, true); // cria ou abre
+                            arquivo.Close();
+                            Console.Clear();
+                            Console.WriteLine("=====================================================================");
+                            Console.WriteLine("ARQUIVO CRIADO.");
+                            Console.WriteLine("=====================================================================");
+                            System.Threading.Thread.Sleep(1000);
+                            Main();
+                        }
+                        break;
+                    case 2:
+                        Console.Clear();
+                        StreamWriter valor = new StreamWriter(path + arquivoEscolhido, true); // abre ou cria
                         Console.WriteLine("=====================================================================");
-                        System.Threading.Thread.Sleep(1000);
-                        Main();
-                    }
-                    else
-                    {
-                        FileStream fs = File.Create(path);
+                        Console.Write("DIGITE ALGO: ");
+                        valor.WriteLine(Console.ReadLine());
+                        valor.Close();
                         Console.Clear();
                         Console.WriteLine("=====================================================================");
-                        Console.WriteLine("ARQUIVO CRIADO.");
+                        Console.WriteLine("Deseja escrever algo mais? <s/n>");
+                        if (Console.ReadLine() == "s")
+                            Acao(2);
+                        else
+                            Main();
+                        break;
+                    case 3:
+                        Console.Clear();
                         Console.WriteLine("=====================================================================");
-                        System.Threading.Thread.Sleep(1000);
-                        Main();
-                    }
-                    break;
-                case 2:
-                    Console.Clear();
-                    var path2 = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + arquivoEscolhido;
-                    StreamWriter valor = new StreamWriter(path2, true); // abre
-                    Console.WriteLine("=====================================================================");
-                    Console.Write("DIGITE ALGO: ");
-                    valor.Write(Console.ReadLine());
-                    valor.Close();
-                    string text = System.IO.File.ReadAllText(path2);
-                    Console.WriteLine(text);
-                    break;
-                case 3:
-                    Console.Clear();
-                    break;
-                default:
-                    break;
+                        string text = System.IO.File.ReadAllText(path + arquivoEscolhido);
+                        Console.Write(text);
+                        Console.WriteLine("=====================================================================");
+                        Console.WriteLine("Retornar? <s/n>");
+                        if (Console.ReadLine() == "s")
+                            Main();    
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("=====================================================================");
+                        Console.Write("Digite o nome do arquivo: ");
+                        arquivoEscolhido = @$"\{Console.ReadLine()}.txt";
+                        if (File.Exists(path + arquivoEscolhido))
+                            Main();
+                        else
+                            Console.Clear();
+                        arquivoEscolhido = @"\Teste.txt";
+                        Console.WriteLine("=====================================================================");
+                        Console.WriteLine("O arquivo não existe, deseja criá-lo? <s/n>");
+                        if (Console.ReadLine() == "s")
+                            Acao(1);
+                        else
+                            Main();
+                        break;
+                }
             }
+            catch (System.Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("=====================================================================");
+                Console.WriteLine("Escolha uma opção valida.");
+                Console.WriteLine("=====================================================================");
+                System.Threading.Thread.Sleep(1000);
+                Main();
+            }
+
         }
     }
 }
