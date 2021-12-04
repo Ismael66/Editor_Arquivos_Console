411c41e55f41eff04d886909054dbc82f0c99a7c
Ismael Mateus <ismaelbnt66@gmail.com>
2021-12-03 23:30:11 -0300

    Refatoração

--- Program.cs
+++ Program.cs
@@ -14,115 +14,133 @@ namespace Criador
             try
             {
                 Console.Clear();
-                Console.WriteLine("=====================================================================");
+                criaLinha();
                 Console.WriteLine("Menu Principal");
-                Console.WriteLine("=====================================================================");
-                Console.WriteLine("[1] CRIAR ARQUIVO");
-                Console.WriteLine("[2] INSERIR DADOS");
-                Console.WriteLine("[3] VISUALIZAR ARQUIVO");
-                Console.WriteLine("[4] MUDAR ARQUIVO");
-                Console.WriteLine("=====================================================================");
-                Console.Write("Escolha: ");
-                int escolha = int.Parse(Console.ReadLine());
-                Acao(escolha);
+                criaLinha();
+                Console.WriteLine("[1] Criar arquivo\n" +
+                "[2] Inserir dados no arquivo\n" +
+                "[3] Visualizar arquivo\n" +
+                "[4] Alterar arquivo selecionado");
+                criaLinha();
+                Console.Write("Digite a opção desejada: ");
+                Acao(Console.ReadLine());
             }
-            catch (System.Exception)
+            catch (System.Exception ex)
             {
-                Console.Clear();
-                Console.WriteLine("=====================================================================");
-                Console.WriteLine("Escolha uma opção valida.");
-                Console.WriteLine("=====================================================================");
-                System.Threading.Thread.Sleep(1000);
-                Main();
+                throw new Exception(ex.Message);
             }
         }
-        public static void Acao(int escolha)
+        static void Acao(string? escolha)
         {
             try
             {
+                Console.Clear();
+                criaLinha();
                 switch (escolha)
                 {
-
-                    case 1:
-                        Console.Clear();
-                        Console.WriteLine("=====================================================================");
-                        Console.Write("ESCOLHA O TITULO DO ARQUIVO: ");
-                        arquivoEscolhido = @$"\{Console.ReadLine()}.txt";
-                        if (File.Exists(path + arquivoEscolhido))
-                        {
-                            Console.Clear();
-                            Console.WriteLine("=====================================================================");
-                            Console.WriteLine("JÁ EXISTE UM ARQUIVO COM O MESMO NOME.");
-                            Console.WriteLine("=====================================================================");
-                            System.Threading.Thread.Sleep(1000);
-                            Main();
-                        }
-                        else
-                        {
-                            StreamWriter arquivo = new StreamWriter(path + arquivoEscolhido, true); // cria ou abre
-                            arquivo.Close();
-                            Console.Clear();
-                            Console.WriteLine("=====================================================================");
-                            Console.WriteLine("ARQUIVO CRIADO.");
-                            Console.WriteLine("=====================================================================");
-                            System.Threading.Thread.Sleep(1000);
-                            Main();
-                        }
+                    case "1":
+                        criaArquivo();
                         break;
-                    case 2:
-                        Console.Clear();
-                        StreamWriter valor = new StreamWriter(path + arquivoEscolhido, true); // abre ou cria
-                        Console.WriteLine("=====================================================================");
-                        Console.Write("DIGITE ALGO: ");
-                        valor.WriteLine(Console.ReadLine());
-                        valor.Close();
-                        Console.Clear();
-                        Console.WriteLine("=====================================================================");
-                        Console.WriteLine("Deseja escrever algo mais? <s/n>");
-                        if (Console.ReadLine() == "s")
-                            Acao(2);
-                        else
-                            Main();
+                    case "2":
+                        insereDados();
                         break;
-                    case 3:
-                        Console.Clear();
-                        Console.WriteLine("=====================================================================");
-                        string text = System.IO.File.ReadAllText(path + arquivoEscolhido);
-                        Console.Write(text);
-                        Console.WriteLine("=====================================================================");
-                        Console.WriteLine("Retornar? <s/n>");
-                        if (Console.ReadLine() == "s")
-                            Main();    
+                    case "3":
+                        visualizaArquivo();
                         break;
-                    case 4:
-                        Console.Clear();
-                        Console.WriteLine("=====================================================================");
-                        Console.Write("Digite o nome do arquivo: ");
-                        arquivoEscolhido = @$"\{Console.ReadLine()}.txt";
-                        if (File.Exists(path + arquivoEscolhido))
-                            Main();
-                        else
-                            Console.Clear();
-                        arquivoEscolhido = @"\Teste.txt";
-                        Console.WriteLine("=====================================================================");
-                        Console.WriteLine("O arquivo não existe, deseja criá-lo? <s/n>");
-                        if (Console.ReadLine() == "s")
-                            Acao(1);
-                        else
-                            Main();
+                    case "4":
+                        alteraArquivo();
+                        break;
+                    default:
+                        retornarMenu("Escolha uma opção valida.", true);
+                        Main();
                         break;
                 }
             }
-            catch (System.Exception)
+            catch (System.Exception ex)
             {
-                Console.Clear();
-                Console.WriteLine("=====================================================================");
-                Console.WriteLine("Escolha uma opção valida.");
-                Console.WriteLine("=====================================================================");
-                System.Threading.Thread.Sleep(1000);
+                throw new Exception(ex.Message);
+            }
+        }
+        #region Funções Switch
+        static void criaArquivo()
+        {
+            Console.Write("Digite um nome para o arquivo: ");
+            arquivoEscolhido = @$"\{Console.ReadLine()}.txt";
+            if (File.Exists(path + arquivoEscolhido))
+            {
+                retornarMenu("Já existe um arquivo come este nome.", true);
                 Main();
             }
-
+            else
+            {
+                var arquivo = new StreamWriter(path + arquivoEscolhido, true); // cria ou abre
+                arquivo.Close();
+                retornarMenu("Arquivo criado com sucesso.", true);
+                Main();
+            }
+        }
+        static void insereDados()
+        {
+            var valor = new StreamWriter(path + arquivoEscolhido, true); // abre ou cria
+            Console.Write("Digite algo: ");
+            valor.WriteLine(Console.ReadLine());
+            valor.Close();
+            if (retornarMenu("Deseja escrever algo mais? <s/n>"))
+                Acao("2");
+            else
+                Main();
+        }
+        static void visualizaArquivo()
+        {
+            string text = System.IO.File.ReadAllText(path + arquivoEscolhido);
+            Console.Write(text);
+            if (retornarMenu("Retornar? <s/n>"))
+                Main();
+        }
+        static void alteraArquivo()
+        {
+            Console.Write("Digite o nome do arquivo: ");
+            arquivoEscolhido = @$"\{Console.ReadLine()}.txt";
+            if (File.Exists(path + arquivoEscolhido))
+                Main();
+            else
+                Console.Clear();
+            arquivoEscolhido = @"\Teste.txt";
+            if (retornarMenu("O arquivo não existe, deseja criá-lo? <s/n>"))
+                Acao("1");
+            else
+                Main();
+        }
+        #endregion
+        #region Funções Comuns
+        static void criaLinha(int repeticoes = 24, char simbolo = '=')
+        {
+            var store = new StringBuilder(repeticoes);
+            int index = 0;
+            while (index < repeticoes)
+            {
+                store.Append(simbolo);
+                index++;
+            }
+            Console.WriteLine(store.ToString());
+        }
+        static bool retornarMenu(string msg = "Ok", bool read = false)
+        {
+            criaLinha();
+            Console.WriteLine(msg + (read ? "\nPressione qualquer tecla para continuar." : string.Empty));
+            if (read)
+            {
+                Console.ReadKey(); // sem enter
+                return false;
+            }
+            string? resposta = Console.ReadLine();
+            Console.Clear();
+            if (!string.IsNullOrEmpty(resposta) &&
+            resposta.ToLower() == "s")
+                return true;
+            else
+                return false;
         }
     }
+    #endregion
 }
